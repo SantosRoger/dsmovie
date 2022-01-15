0981dbf6db2b3a6aa4c0d5edc72af0e92b02b7da
SantosRoger <santosroger525@gmail.com>
2022-01-15 11:47:56 -0300

    _redirects

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-template-curly-in-string */
 import MovieScore from "components/MovieScore";
 import { Link } from "react-router-dom";
 
@@ -15,7 +16,9 @@ function MovieCard() {
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
+               
                 <MovieScore />
+                              
                 <Link to={'/form/${movie.id}'}>
                     <div className="btn btn-primary dsmovie-btn">Avaliar</div>
                 </Link>
